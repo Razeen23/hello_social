a70c0788a7f5db19fcd0b66e2152a5d525419bd4
razeen <makerrazeen2301@gmail.com>
2025-02-13 03:12:14 +0530

    13

--- src/page/Hero.tsx
+++ src/page/Hero.tsx
@@ -40,6 +40,8 @@ const Hero = () => {
                     initial={{ opacity: 0, scale: 0.8 }}
                     animate={{ opacity: 1, scale: 1 }}
                     transition={{ duration: 0.5, delay: 0.6 }}
+                    onClick={() => document.getElementById("about")?.scrollIntoView({ behavior: "smooth" })}
+
                 >
                     Know More
                 </motion.button>
